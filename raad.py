747864feb30b16efce3ee8b9fe469f0caa50dd3f
Jsyr963 <89968301+Jsyr963@users.noreply.github.com>
2021-11-28 15:56:26 +0100

    Eerste commit!

---
+++ raad.py
@@ -0,0 +1,12 @@
+#John Youssef
+import random
+Winnendgetal = random.randint(1, 1000)
+print(Winnendgetal)
+for i in range(20):
+    Winnendgetal
+    for i in range(10):
+        gekozengetal=input("Typ een getal in:")
+        if gekozengetal == Winnendgetal:
+            print("Goed Gedaan!")
+        else:
+            print("Jammer, Probeer nog een keer!")
